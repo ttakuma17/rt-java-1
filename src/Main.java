3cf9f7eccea3d0813f4276a301127601551f4153
ttakuma17 <ttanimiya@gmail.com>
2022-12-21 23:38:30 +0900

    Hello world カスタマイズ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args){
         System.out.println("Hello world!");
+//        for(int i = 0; i < 10; i++){
+//            System.out.println("Hello world!");
+//        }
+//        for(int i = 0; i < 20; i++){
+//            if(i < 10) System.out.println("Hello world!");
+//            else {
+//                if(i % 2 == 0) System.out.println("Hello");
+//                else System.out.println("World");
+//            }
+//        }
     }
 }
